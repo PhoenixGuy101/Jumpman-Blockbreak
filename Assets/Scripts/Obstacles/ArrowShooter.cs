65d13ca9eae1b4edb8354f3ab9d41e153d01a88e
Luka <lhettenkofer@yahoo.com>
2023-08-01 19:20:15 -0700

    Framework for the ArrowShooter script set up

---
+++ Assets/Scripts/Obstacles/ArrowShooter.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowShooter : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 arrowSpeed;
+    [SerializeField]
+    private float shootDelay = 2;
+    private float shootTimer;
+    [SerializeField]
+    private GameObject arrowPrefab;
+
+    private void Start()
+    {
+        shootTimer = shootDelay;
+    }
+
+    private void FixedUpdate()
+    {
+        if (shootTimer > 0) shootTimer -= Time.fixedDeltaTime;
+        else
+        {
+            ShootArrow(); //will have to trigger an animation instead, which then triggers the actual shooting of the arrow upon completion.
+        }
+    }
+
+    private void ShootArrow()
+    {
+        GameObject instArrow = Instantiate(arrowPrefab, transform.position, Quaternion.identity);
+        instArrow.TryGetComponent(out IProjectile iProj);
+        if (iProj != null)
+        {
+            iProj.Setup(arrowSpeed);
+        }
+        shootTimer = shootDelay;
+    }
+}
